461ad575832cb90e9cce4b371256a681bba85cad
e-lol <116045805+e-lol@users.noreply.github.com>
2022-10-18 11:17:21 +1000

    Add files via upload

---
+++ basic egg hive.lua
@@ -0,0 +1,38 @@
+
+-- screen cover
+
+local HttpService = game:GetService("HttpService")
+local asset_loading_function = getsynasset or getcustomasset
+local ui = Instance.new("ScreenGui", game:GetService("CoreGui"))
+
+local frame1 = Instance.new("Frame", ui)
+frame1.Size = UDim2.new(1,0,1.1,0)
+frame1.Position = UDim2.new(0,0,-0.1,0)
+frame1.BackgroundTransparency = 0
+frame1.BackgroundColor3 = Color3.new(0,0,0)
+frame1.Draggable = false
+
+if asset_loading_function then
+    local file_name = HttpService:GenerateGUID(false)
+    writefile(file_name..".png", game:HttpGet("https://i.ytimg.com/vi/wQKpdVsoAOc/maxresdefault.jpg"))
+
+    local img = Instance.new("ImageLabel", frame1)
+    img.Size = UDim2.new(1, 0, 1, 0)
+    img.Active = true
+    img.Image = asset_loading_function(file_name..".png")
+
+    task.wait(0.5)
+
+    delfile(file_name..".png") 
+end
+
+-- hive changes
+
+local args1 = {["Type"] = "Basic",["Amount"] = 50,["Category"] = "Eggs"}
+game:GetService("ReplicatedStorage").Events.ItemPackageEvent:InvokeServer("Purchase", args1)
+
+for vertical=1, 10 do
+    for horizontal=1, 5 do
+        game:GetService("ReplicatedStorage").Events.ConstructHiveCellFromEgg:InvokeServer(horizontal, vertical, "Basic", 1, false); task.wait()
+    end
+end
